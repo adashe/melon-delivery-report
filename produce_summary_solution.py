001b8d92d085f48a25a2b65b99e3f77ef7dec80d
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-02-06 04:10:14 +0000

    Added file

---
+++ produce_summary_solution.py
@@ -0,0 +1,13 @@
+
+create dictionary to keep track of item, quantity, total
+
+open each folder
+read each line
+add items to dictionary
+
+
+
+open each dictionary and unpack contents
+print summary statements
+
+
